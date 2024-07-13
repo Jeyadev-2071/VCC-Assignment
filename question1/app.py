492f7e89e1f4920c7b4c6c0574969b6c40c19153
Jeyadev L <74133351+Jeyadev42@users.noreply.github.com>
2024-07-13 22:06:03 +0530

    Major Update to Question 1 app.py code

--- question1/app.py
+++ question1/app.py
@@ -1,5 +1,3 @@
-
-#mysql://jd:jeyadev@172.19.240.109:3306/development
 import streamlit as st
 import mysql.connector
 import pandas as pd
@@ -21,16 +19,25 @@ def create_connection(host_name, user_name, user_password, db_name):
 
 # Function to execute a query and fetch data
 def run_query(connection, query):
-    st.write(sql_query)
-    st.write(query)
     cursor = connection.cursor(dictionary=True)
     try:
         cursor.execute(query)
         result = cursor.fetchall()
         cursor.close()
-    except:
-        st.error(f'Failed to run the query - {query}')
-    return result
+        return result
+    except mysql.connector.Error as err:
+        st.error(f"Failed to run the query - {err}")
+        return None
+
+# Function to fetch data from the selected table
+def fetch_data(connection, table_choice):
+    sql_query = f'SELECT * FROM {table_choice};'
+    tbl_data = run_query(connection, query=sql_query)
+    if tbl_data:
+        df = pd.DataFrame(tbl_data)
+        st.table(df)
+    else:
+        st.write("No data found or query failed.")
 
 # Streamlit interface
 st.title('MySQL Database Connector')
@@ -42,32 +49,20 @@ user = st.sidebar.text_input('User', 'root')
 password = st.sidebar.text_input('Password', type='password')
 database = st.sidebar.text_input('Database')
 
+if 'connection' not in st.session_state:
+    st.session_state.connection = None
 
+if st.sidebar.button('Connect'):
+    st.session_state.connection = create_connection(host, user, password, database)
 
-st.header('Query the Database')
-sql_query = st.text_input('SQL Query ', 'SELECT * FROM "Enter your table name";')
+if st.session_state.connection:
+    table_lst_data = run_query(st.session_state.connection, query='SHOW TABLES;')
+    if table_lst_data:
+        df_lst_tbl = pd.DataFrame(table_lst_data)
+        options = ['Select an option'] + df_lst_tbl[df_lst_tbl.columns[0]].tolist()
+        table_choice = st.selectbox(label='Select Table from the below list', options=options)
 
-if st.button('Run Query'):
-    try:
-        connection = create_connection(host, user, password, database)
-        data = run_query(connection, query=sql_query)   
-        
-        if data:
-            df = pd.DataFrame(data)
-            st.table(df)
-        else:
-            st.write("No data found or query failed.")
-    except:
-        st.error('Not Able make connect to database due to invalid crendentials')
-  
-    
-    # if st.button('Run Query'):
-    #     try:    
-    #         data = run_query(connection, query)
-    #         if data:
-    #             df = pd.DataFrame(data)
-    #             st.write(df)
-    #         else:
-    #             st.write("No data found or query failed.")
-    #     except:
-    #         st.warning('Query Execution Failed')
+        if table_choice and table_choice != 'Select an option':
+            fetch_data(st.session_state.connection, table_choice)
+    else:
+        st.write("Failed to retrieve tables from the database.")
